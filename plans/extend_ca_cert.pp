944e74e38cd5117b94cf3d557f1132be14cfda26
Taika Augustaitis <taika.augustaitis@puppet.com>
2023-04-06 12:52:30 -0400

    (SUP-3564)
    This commit adds support for PE installations with Disaster Recovery
    (DR) enabled, modifies the code to use systemd instead of run_task to
    stop and start services, stops and starts all PE services to load the
    new CA, and attempts to protect against temporary
    service disruptions due to running puppet infra configure --no-recover.
    
    Prior to this commit configuring a replica to use the extended CA was a
    manual process. Furthermore, sometimes services would fail to start on
    compilers until Puppet was run on the primary server to
    update configuration files such as pg_ident.conf and
    infra_inventory.txt. Futhermore, only several services were stopped and
    started on infra nodes to load the new CA content explicitly.
    
    A replica parameter was added to the ca_extend::extend_ca_cert plan to
    support PE installs with DR enabled. Using systemd to stop and start
    services allows the ca_extend::extend_ca_cert plan to stop and start
    Puppet services with one run_command function instead of multiple
    run_task functions. This also enables using globbing to allow the same
    code to support PE compilers with PuppetDB and legacy compilers without
    added complexity. The addition of the Puppet run on the primary server
    after Configuring <target> to use the extended CA certificate updates
    configuration files changed by running puppet infra configure
    --no-recover within the ca_extend::configure_primary task.

--- plans/extend_ca_cert.pp
+++ plans/extend_ca_cert.pp
@@ -1,8 +1,9 @@
 # @summary
-#   Plan that extends the Puppet CA certificate and configures the primary Puppet server
+#   Plan that extends the Puppet CA certificate and configures the primary Puppet server, Replica
 #   and Compilers to use the extended certificate.
-# @param targets The target node on which to run the plan.  Should be the primary Puppet server
-# @param compilers Optional comma separated list of compilers to upload the certificate to
+# @param targets The target node on which to run the plan. Should be the primary Puppet server
+# @param compilers Optional comma separated list of compilers to configure to use the extended CA
+# @param replica Optional replica to configure to use the extended CA 
 # @param ssldir Location of the ssldir on disk
 # @param regen_primary_cert Whether to also regenerate the agent certificate of the primary Puppet server
 # @example Extend the CA cert and regenerate the primary agent cert locally on the primary Puppet server
@@ -12,6 +13,7 @@
 plan ca_extend::extend_ca_cert(
   TargetSpec $targets,
   Optional[TargetSpec] $compilers = undef,
+  Optional[TargetSpec] $replica = undef,
   $ssldir                               = '/etc/puppetlabs/puppet/ssl',
   $regen_primary_cert                   = false,
 ) {
@@ -20,11 +22,9 @@ plan ca_extend::extend_ca_cert(
 
   if $primary_facts['pe_build'] {
     $is_pe = true
-    $services = ['puppet', 'pe-puppetserver', 'pe-postgresql']
   }
   elsif $primary_facts['puppetversion'] {
     $is_pe = false
-    $services = ['puppet', 'puppetserver']
   }
   else {
     fail_plan("Puppet not detected on ${targets}")
@@ -46,12 +46,15 @@ plan ca_extend::extend_ca_cert(
       out::message('INFO: CRL expired, truncating to regenerate')
       run_task('ca_extend::crl_truncate', $targets)
     }
+    out::message("INFO: Stopping Puppet services on ${targets}")
+    run_command('/bin/systemctl stop pe-* puppet pxp-agent', $targets)
   }
 
-  out::message("INFO: Stopping Puppet services on ${targets}")
-  $services.each |$service| {
-    run_task('service::linux', $targets, 'action' => 'stop', 'name' => $service)
+  else {
+    out::message("INFO: Stopping Puppet services on ${targets}")
+    run_command('/bin/systemctl stop puppet puppetserver', $targets)
   }
+
 
   out::message("INFO: Extending CA certificate on ${targets}")
   $regen_results = run_task('ca_extend::extend_ca_cert', $targets)
@@ -67,24 +70,42 @@ plan ca_extend::extend_ca_cert(
   else {
     run_command("/bin/cp ${new_cert['new_cert']} ${ssldir}/certs/ca.pem", $targets)
     run_command("/bin/cp ${new_cert['new_cert']} ${ssldir}/ca/ca_crt.pem", $targets)
-    run_task('service::linux', $targets, 'action' => 'start', 'name' => 'puppetserver')
+    run_command('/bin/systemctl start puppet puppetserver', $targets)
   }
-  run_task('service::linux', $targets, 'action' => 'start', 'name' => 'puppet')
 
   $tmp = run_command('mktemp', 'localhost', '_run_as' => system::env('USER'))
   $tmp_file = $tmp.first.value['stdout'].chomp
   file::write($tmp_file, $cert_contents)
 
+  run_command('/opt/puppetlabs/bin/puppet agent --no-daemonize --no-noop --onetime', $targets)
+
+  if $is_pe and $replica {
+    out::message("INFO: Stopping Puppet services on the replica (${replica})")
+    run_command('/bin/systemctl stop pe-* puppet pxp-agent', $replica)
+    out::message("INFO: Configuring the replica (${replica}) to use the extended CA certificate")
+    upload_file($tmp_file, '/etc/puppetlabs/puppet/ssl/certs/ca.pem', $replica)
+    run_command('/opt/puppetlabs/bin/puppet agent --no-daemonize --no-noop --onetime', $replica)
+    run_command('/bin/systemctl start pe-* puppet pxp-agent', $replica)
+  }
+
   if $compilers {
-    out::message("INFO: Stopping Puppet services on compilers (${compilers})")
-    run_task('service::linux', $compilers, 'action' => 'stop', 'name' => 'puppet')
+    if $is_pe {
+      out::message("INFO: Stopping Puppet services on compilers (${compilers})")
+      run_command('/bin/systemctl stop pe-* puppet pxp-agent', $compilers)
+      out::message("INFO: Configuring compilers (${compilers}) to use the extended CA certificate")
+      upload_file($tmp_file, '/etc/puppetlabs/puppet/ssl/certs/ca.pem', $compilers)
+      run_command('/opt/puppetlabs/bin/puppet agent --no-daemonize --no-noop --onetime', $compilers)
+      run_command('/bin/systemctl start pe-* puppet pxp-agent', $compilers)
+    }
+    else {
+      out::message("INFO: Stopping Puppet services on compilers (${compilers})")
+      run_command('/bin/systemctl stop puppet puppetserver', $compilers)
+      out::message("INFO: Configuring compilers (${compilers}) to use the extended CA certificate")
+      upload_file($tmp_file, '/etc/puppetlabs/puppet/ssl/certs/ca.pem', $compilers)
+      run_command('/opt/puppetlabs/bin/puppet agent --no-daemonize --no-noop --onetime', $compilers)
+      run_command('/bin/systemctl start puppet puppetserver', $compilers)
+    }
 
-    out::message("INFO: Configuring compilers (${compilers}) to use the extended CA certificate")
-    upload_file($tmp_file, '/etc/puppetlabs/puppet/ssl/certs/ca.pem', $compilers)
-
-    # Just running Puppet with the new CA certificate in place should be enough.
-    run_command('/opt/puppetlabs/bin/puppet agent --no-daemonize --no-noop --onetime', $compilers)
-    run_task('service::linux', $compilers, 'action' => 'start', 'name' => 'puppet')
   }
 
   out::message("INFO: Extended CA certificate decoded and stored at ${tmp_file}")
